392e7cbd3d021dc4127540677a81e2dc55cc08ff
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-08-16 18:12:17 -0300

    feat: carregar ponte de prontidão operacional

--- src/integration/product-extensions-bootstrap.js
+++ src/integration/product-extensions-bootstrap.js
@@ -19,7 +19,8 @@
         '/src/integration/controller-guide.js',
         '/src/integration/controller-guide-ready.js',
         '/src/integration/unidentified-expense-ux.js',
-        '/src/integration/prontuario-operational-ux.js'
+        '/src/integration/prontuario-operational-ux.js',
+        '/src/integration/operational-readiness-bridge.js'
     ]);
 
     function loadStyleOnce(href) {
