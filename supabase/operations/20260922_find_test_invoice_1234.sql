ca7d9eb3f377e0d9014ae05796af2c2bcea0f53b
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-09-22 15:02:37 -0300

    ops: confirmar remoção integral do lançamento teste 1234

--- supabase/operations/20260922_find_test_invoice_1234.sql
+++ supabase/operations/20260922_find_test_invoice_1234.sql
@@ -1,22 +1,15 @@
 select
-  i.id,
-  i.school_id,
-  i.competence_id,
-  i.program_id,
-  i.verification_id,
-  i.expense_type,
-  i.invoice_number,
-  i.description,
-  i.amount,
-  i.linked_asset_id,
-  i.row_version,
-  i.payload,
-  (select count(*) from public.pendencies p where p.registered_invoice_id=i.id) as pendencias_vinculadas
-from public.registered_invoices i
-where i.id='nota-8f25ad3a-da84-4867-a1cf-e2acbfbd2fdd';
-
-select
-  count(*) as similar_count
-from public.registered_invoices i
-where lower(i.invoice_number) like '%1234%teste%'
-   or lower(i.description) like '%limpeza%caixa%';
+  (select count(*) from public.registered_invoices where id='nota-8f25ad3a-da84-4867-a1cf-e2acbfbd2fdd') as invoice_by_id,
+  (select count(*) from public.registered_invoices
+     where lower(invoice_number) like '%1234%teste%'
+        or lower(description) like '%limpeza%caixa%') as similar_invoices,
+  (select count(*) from public.pendencies where id='pend-4c35085f-3b5a-4077-bc23-bf5ceecfafde') as pendency_by_id,
+  (select count(*) from public.pendency_attempts where pendency_id='pend-4c35085f-3b5a-4077-bc23-bf5ceecfafde') as attempts_by_pendency,
+  (select count(*) from public.pendency_contacts where pendency_id='pend-4c35085f-3b5a-4077-bc23-bf5ceecfafde') as contacts_by_pendency,
+  (select count(*) from public.administrative_logs where id='log-cleanup-test-invoice-1234-20260922') as cleanup_log_count,
+  (select row_version from public.verifications where id='04.10.001::2026-09::BASIC') as verification_row_version,
+  (select analysis ->> 'notaFiscal' from public.verifications where id='04.10.001::2026-09::BASIC') as nota_fiscal_analysis,
+  (select analysis ->> 'consAssessoria' from public.verifications where id='04.10.001::2026-09::BASIC') as assessoria_analysis,
+  (select bonification ->> 'notaFiscal' from public.verifications where id='04.10.001::2026-09::BASIC') as nota_fiscal_bonification,
+  (select bonification ->> 'consAssessoria' from public.verifications where id='04.10.001::2026-09::BASIC') as assessoria_bonification,
+  (select bonification ->> 'consEnviada' from public.verifications where id='04.10.001::2026-09::BASIC') as assessoria_sent;
